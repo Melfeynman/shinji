0080a3440768c93e0c8319de081f91e44cb56c50
hdekjsne <hdekjsne@gmail.com>
2023-06-19 08:34:11 +0300

    added grid simulation for a map

--- src/scene/game.js
+++ src/scene/game.js
@@ -10,20 +10,35 @@ export default class Game extends Phaser.Scene {
   }
 
   background;
+  gridCoordinates = { x: [0], y: [0] };
 
   preload() {
     this.load.image('background', 'assets/test-image.jpg');
   }
 
   create() {
-    this.background = this.add.image(Math.round(vpwidth / 2) , Math.round(vpheight / 2), 'background')
+    this.background = this.add.image(Math.round(vpwidth / 2) , Math.round(vpheight / 2), 'background');
   }
 
   update() {
     this.resizeBg(this.background);
+    this.makeGrid();
   }
 
   resizeBg(bg) {
     vpwidth > vpheight ? bg.setDisplaySize(vpwidth, vpwidth) : bg.setDisplaySize(vpheight, vpheight);
+  }
+
+  makeGrid() {
+    const columnsCount = Math.floor(vpwidth / blockSize) + 1;
+    const rowsCount = Math.floor(vpheight / blockSize) + 1;
+    let steps = 0;
+    for (let i = 1; i <= columnsCount; i++) {
+      this.gridCoordinates.x.push(steps += blockSize);
+    }
+    steps = 0;
+    for (let i = 1; i <= rowsCount; i++) {
+      this.gridCoordinates.y.push(steps += blockSize);
+    }
   }
 }
