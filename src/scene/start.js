30e722de500cd028d276b281df76c452e9eceb24
User-1 <hdekjsne@gmail.com>
2023-06-27 15:11:43 +0300

    show #main-menu at Start

--- src/scene/start.js
+++ src/scene/start.js
@@ -1,7 +1,13 @@
 import Phaser from '../lib/phaser.js';
 
+const mainMenu = document.querySelector('#main-menu');
+
 export default class Start extends Phaser.Scene {
   constructor() {
     super('start');
+  }
+
+  create() {
+    mainMenu.style.display = 'flex';
   }
 }
