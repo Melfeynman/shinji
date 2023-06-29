bdf6764c28d75b1c79becb84563a38af3322f331
hilraen <hilraen@yandex.ru>
2023-06-30 00:13:10 +0300

    score update

--- src/scene/game-over.js
+++ src/scene/game-over.js
@@ -16,14 +16,16 @@ export default class Over extends Phaser.Scene {
 
   create() {
     //console.log('i am here');
+    const test = $finalScore.textContent;
+    $finalScore.textContent = this.finalScore;
     const vpwidth = window.innerWidth;
     const vpheight = window.innerHeight;
 
     /*тут после add.text первымы двумя параметрами должны идти параметры расположения по иску и игрику, 
     по какой то причине когда эти параметры заезжают в то место где они должны быть (в центре) счет пропадает, хотя если поставить параметры
-    рандомные, например 10 и 10 счет становится виден*/
+    рандомные, например 10 и 10 счет становится виден
     this.txt_score = this.add.text(vpwidth / 2, vpheight / 2, 'Ваш счет: ' + this.finalScore, { fontSize: "70px Arial", fill: '#fff'});
-    this.txt_score.depth = 100;
+    this.txt_score.depth = 100;*/
     endScreen.style.display = 'flex';
     restartBtn.addEventListener('click', () => {
       this.scene.start('start');
